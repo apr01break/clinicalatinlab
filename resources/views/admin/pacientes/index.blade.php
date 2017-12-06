711ddbc626b8cc9d261fb5a8bd49fb58c5626efb
AlexisHolyoak <peraltaholyoak@gmail.com>
2017-12-05 22:40:43 -0500

    prototipo del crud para pacientes

--- resources/views/admin/pacientes/index.blade.php
+++ resources/views/admin/pacientes/index.blade.php
@@ -1,12 +1,17 @@
 @extends('admin.layouts.admin')
 
 @section('title', 'Pacientes')
+@section('title-right')
+<div>
+  <a href="{{route('pacientes.create')}}" class="btn btn-primary">Registrar paciente</a>
+</div>
+@endsection
 @section('content')
 <div class="row">
   <div class="col-md-12 col-sm-12 col-xs-12">
     <div class="x_panel">
       <div class="x_title">
-        <h2>Registro de pacientes</h2>
+        <h2>Registros de pacientes</h2>
         <ul class="nav navbar-right panel-toolbox">
           <li>
             <a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
@@ -22,7 +27,7 @@
           <div class="dataTables_wrapper form-inline dt-bootstrap no-footer" id="datatable_wrapper">
             <div class="row">
               <div class="table-responsive">
-                <table id="t_pacientes" class="table table-striped table-bordered dt-responsive nowrap" cellspacing="0" width="100%">
+                <table id="t_data_tables" class="table table-striped table-bordered dt-responsive nowrap" cellspacing="0" width="100%">
                   <thead>
                     <tr>
                       <th data-priority="1">Nombre</th>
@@ -30,7 +35,7 @@
                       <th>DNI</th>
                       <th>Fecha de Nacimiento</th>
                       <th>Fecha de Registro</th>
-                      <th data-priority="2">Acciones</th>
+                      <th data-priority="2" width="1%">Acciones</th>
                     </tr>
                   </thead>
                   <tbody>
@@ -40,9 +45,9 @@
                       <td>73237155</td>
                       <td>24/10/1997</td>
                       <td>20/20/1990</td>
-                      <td><a href="#" class="btn btn-info btn-xs"><i class="fa fa-eye"></i></a>
-                          <a href="#" class="btn btn-warning btn-xs"><i class="fa fa-pencil"></i></a>
-                          <a href="#" class="btn btn-danger btn-xs"><i class="fa fa-close"></i></a>
+                      <td><a href="{{route('pacientes.show')}}" class="btn btn-info btn-xs"><i class="fa fa-eye"></i></a>
+                          <a href="{{route('pacientes.update')}}" class="btn btn-warning btn-xs"><i class="fa fa-pencil"></i></a>
+                          <a href="#" class="btn btn-danger btn-xs" data-toggle="modal" data-target="#bs-delete-modal-lg"><i class="fa fa-close"></i></a>
                       </td>
                     </tr>
                     <tr>
@@ -51,9 +56,9 @@
                       <td>73237155</td>
                       <td>24/10/1997</td>
                       <td>20/20/1990</td>
-                      <td><a href="#" class="btn btn-info btn-xs"><i class="fa fa-eye"></i></a>
-                          <a href="#" class="btn btn-warning btn-xs"><i class="fa fa-pencil"></i></a>
-                          <a href="#" class="btn btn-danger btn-xs"><i class="fa fa-close"></i></a>
+                      <td><a href="{{route('pacientes.show')}}" class="btn btn-info btn-xs"><i class="fa fa-eye"></i></a>
+                          <a href="{{route('pacientes.update')}}" class="btn btn-warning btn-xs"><i class="fa fa-pencil"></i></a>
+                          <a href="#" class="btn btn-danger btn-xs" data-toggle="modal" data-target="#bs-delete-modal-lg"><i class="fa fa-close"></i></a>
                       </td>
                     </tr>
                     <tr>
@@ -62,9 +67,9 @@
                       <td>73237155</td>
                       <td>24/10/1997</td>
                       <td>20/20/1990</td>
-                      <td><a href="#" class="btn btn-info btn-xs"><i class="fa fa-eye"></i></a>
-                          <a href="#" class="btn btn-warning btn-xs"><i class="fa fa-pencil"></i></a>
-                          <a href="#" class="btn btn-danger btn-xs"><i class="fa fa-close"></i></a>
+                      <td><a href="{{route('pacientes.show')}}" class="btn btn-info btn-xs"><i class="fa fa-eye"></i></a>
+                          <a href="{{route('pacientes.update')}}" class="btn btn-warning btn-xs"><i class="fa fa-pencil"></i></a>
+                          <a href="#" class="btn btn-danger btn-xs" data-toggle="modal" data-target="#bs-delete-modal-lg"><i class="fa fa-close"></i></a>
                       </td>
                     </tr>
                   </tbody>
@@ -77,21 +82,32 @@
     </div>
   </div>
 </div>
+@include('admin.pacientes.destroy')
 @endsection
 
 @section('style')
+<!--DataTable-->
 <link rel="stylesheet" href="{{asset('css/dataTables.bootstrap.min.css')}}">
 <link rel="stylesheet" href="{{asset('css/responsive.bootstrap.min.css')}}">
+<!--nprogress-->
+<link rel="stylesheet" href="{{asset('css/nprogress.css')}}">
+<!--Custom theme styles-->
+<link rel="stylesheet" href="{{asset('css/custom.min.css')}}">
 @endsection
+
 @section('script')
+<!--DataTable-->
 <script type="text/javascript" src="{{asset('js/jquery.dataTables.min.js')}}"></script>
 <script type="text/javascript" src="{{asset('js/dataTables.bootstrap.min.js')}}"></script>
 <script type="text/javascript" src="{{asset('js/dataTables.responsive.min.js')}}"></script>
 <script type="text/javascript" src="{{asset('js/responsive.bootstrap.min.js')}}"></script>
+<!--bootstrap-datepicker-->
+<script type="text/javascript" src="{{asset('js/bootstrap-datepicker.min.js')}}"></script>
+<!--nprogress-->
+<script type="text/javascript" src="{{asset('js/nprogress.js')}}"></script>
+<!--Custom theme styles-->
 <script type="text/javascript" src="{{asset('js/custom.min.js')}}"></script>
 <script type="text/javascript">
-$(document).ready(function() {
-    $('#t_pacientes').DataTable();
-});
+  $(document).ready(function(){$("#t_data_tables").DataTable()});
 </script>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -34,7 +34,16 @@ Route::get('prueba',function(){
   return view('admin.home.index');
 });
 Route::group(['prefix'=>'pacientes','as'=>'pacientes.'],function(){
-  Route::get('index',function(){
+  Route::get('/',function(){
     return view('admin.pacientes.index');
   })->name('index');
+  Route::get('create',function(){
+    return view('admin.pacientes.create');
+  })->name('create');
+  Route::get('update',function(){
+    return view('admin.pacientes.update');
+  })->name('update');
+  Route::get('show',function(){
+    return view('admin.pacientes.show');
+  })->name('show');
 });
