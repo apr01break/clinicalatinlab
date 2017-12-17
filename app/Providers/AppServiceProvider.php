43f250a8734b4e7a59481abaf2c44f2acf4ae372
AlexisHolyoak <peraltaholyoak@gmail.com>
2017-12-17 15:46:56 -0500

    getenv

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-      if (env('APP_ENV') === 'production') {
+      if (getenv('APP_ENV') === 'production') {
         $url->forceScheme('https');
       }
     }
